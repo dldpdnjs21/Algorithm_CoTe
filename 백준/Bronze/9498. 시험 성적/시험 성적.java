c922bda5bee1ccecf3e5533f204a2ee7347d8a43
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-07-31 14:16:11 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(90 <= N && N <= 100){
+            System.out.println("A");
+        } else if(80 <= N && N <= 89){
+            System.out.println("B");
+        } else if(70 <= N && N <= 79){
+            System.out.println("C");
+        } else if(60 <= N && N <= 69){
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
