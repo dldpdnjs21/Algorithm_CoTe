6017d46d0791cfe72a6337974c835df16ffec461
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-01 11:23:04 +0900

    [Bronze V] Title: 윤년, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,15 @@
-import java.util.*;
 import java.io.*;
+
 public class Main {
-	
-	static int N;
-	static int sum = 0;
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt(); 
-		
-		if(((N%4==0) && (N%100!=0)) || (N%400==0)){
-			System.out.println("1");
-		}else 
-			System.out.println("0");
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N % 4 == 0 && N % 100 != 0 || N % 400 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
 }
