eee9ac95de130ce15341fd111141b77a51bddc28
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-06 20:04:10 +0900

    [Silver V] Title: 숫자 카드, Time: 972 ms, Memory: 194624 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if(hash.get(input) == null) hash.put(input, 1);
+            else hash.put(input, hash.get(input) + 1);
+        }
+
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if(hash.get(input) == null) sb.append(0).append(" ");
+            else sb.append(hash.get(input)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
